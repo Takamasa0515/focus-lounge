5cf837eadf57f7c4e2fb25bd872bb8ed1d69dcb3
Takamasa0515 <mhe7x0i7l7e@icloud.com>
2026-01-09 10:08:23 +0900

    work_sessionsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     sessions: "users/sessions"
   }
   resources :users, :only => [:show]
+  resources :work_sessions, :only => [:index, :new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
