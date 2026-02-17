bebb0a9cae7ea2f33cf046922a242575a222a591
Takamasa0515 <mhe7x0i7l7e@icloud.com>
2026-02-17 13:10:27 +0900

    ダッシュボードのカテゴリー選択が、すべてのカテゴリーになっていたため、ユーザーが作成したカテゴリーから選択するように変更

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,13 @@
 class DashboardsController < ApplicationController
+  before_action :set_user
   def show
-    @categories = Category.all
+    @categories = Category.where(user_id: @user.id)
     @today_total_seconds = current_user.work_sessions.today.sum(:duration_seconds)
+  end
+
+    private
+
+  def set_user
+    @user = current_user
   end
 end
